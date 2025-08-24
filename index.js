b4c0558f65acac4f54c1a230cf3e217186f4f009
Bruhh1369 <bruhh136969@gmail.com>
2025-08-24 14:41:43 +0700

    v

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let projects = [
     {
         title: "Expanding Cards",
-        link: "expanding-cards/main.html"
+        link: "expanding-cards/main.html",
+        deployedLink: "https://50-projects-pi.vercel.app/expanding-cards/main.html"
     }
 ]
 
 document.getElementById("grid").innerHTML = projects.map(itm => {
     return `
         <a class="card" id="cards" href=${itm.link}>
+            <iframe src=${itm.deployedLink}</iframe>
             <h2>${itm.title}</h2>
         </a>
     `
