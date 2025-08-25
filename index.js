8ce634f4e4900bd6f4e63401d42cee7b548e8c62
Bruhh1369 <bruhh136969@gmail.com>
2025-08-26 05:52:01 +0700

    ytyu

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ let projects = [
 document.getElementById("grid").innerHTML = projects.map(itm => {
     return `
         <a class="card" id="cards" href=${itm.link}>
-        <h2>${itm.title}</h2>
-        <iframe src="${itm.link || itm.deployedLink}"</iframe>
+            <h2>${itm.title}</h2>
+            <iframe src="${itm.link || itm.deployedLink}"</iframe>
         </a>
     `
 }).join("")
